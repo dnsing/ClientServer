2e401fa78246324fc363736bcf95295bed7a90f0
dnsing <33098069+dnsing@users.noreply.github.com>
2018-09-12 01:10:57 -0600

    Initial commit

---
+++ src/Client1.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+//import java.net.*;
+//import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class Client1 {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		JSONObject obj = new JSONObject();
+		obj.put("X1", "numero en X");
+		obj.put("Y1", "numero en Y");
+	 
+		 try (FileWriter file = new FileWriter("test1.json")) {
+
+	            file.write(obj.toJSONString());
+	            file.flush();
+	            
+		  } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	       // System.out.print(obj);
+
+	    
+	            
+		Socket s = new Socket("127.0.0.1",70);
+		System.out.println("AK7");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintStream ps = new PrintStream(s.getOutputStream());
+		BufferedReader brs = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		while(true) {
+			System.out.println("mandelo");
+			String st = br.readLine();
+			ps.println(obj);
+			
+			if(s.equals("exit")) {
+				System.exit(1);
+			}
+			System.out.println("Data returned " + s);
+		}
+
+	}
+
+}
---
+++ src/Server.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+	public static ServerSocket s1;
+	public static ServerSocket s2;
+	
+	public static Socket ss;
+	public static Socket ss2;
+	public static void main(String args[]) throws IOException {		
+		
+		s1 = new ServerSocket(69);
+		ss = s1.accept();
+		System.out.println("CLiente0" + "Esperando J1");
+
+		s2 = new ServerSocket(70);
+		ss2 = s2.accept();
+		System.out.println("CLiente1");
+		
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		if(number==0) {
+			Server.C0();
+		}
+		else {
+			System.out.println("tengo sueno");
+		}
+	}
+	public static void C0() throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(ss.getInputStream()));
+		while(true) {
+			Object s = br.readLine();
+			if(s.equals("exit")==true) {
+				System.exit(1);
+			}
+			System.out.println("mensaje cliente " + s);
+			
+		}
+	}
+
+}
